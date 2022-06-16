830b0c8c5985cfb9d6650c7ba74f647e421d035e
SaidP <perez.sierra.said.ulises@gmail.com>
2022-06-16 18:12:22 -0500

    Primer Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 
- import { saludar } from './js/componentes.js';
+
+
 import './styles.css';
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Said';
+export const todoList = new TodoList();
 
- saludar(nombre);
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
+console.log('todos', todoList.todos);
